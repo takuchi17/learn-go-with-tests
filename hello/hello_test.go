6a2ac5db4d0844df03c28cbae300e446cd0878dd
chihaya <takuchi.1071880@gmail.com>
2025-03-29 16:36:04 +0900

    uodate: 名前が空文字の時のエラーハンドイングの追加とリファクタリング

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,10 +5,21 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris!"
-
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper() //テストのヘルパー関数を示す。これがないとテスト失敗時にヘルパー関数の行数を出力してしまう。
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
