695f6c01e45001940cf0b37c866a4b91d802d739
chihaya <takuchi.1071880@gmail.com>
2025-03-29 16:58:24 +0900

    refactor: Hello関数から言語分岐処理を切り出した

--- hello/hello.go
+++ hello/hello.go
@@ -4,19 +4,36 @@ import (
 	"fmt"
 )
 
-const spanish = "Spanish"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const ending = "!"
+const SPANISH = "Spanish"
+const FRENCH = "French"
+
+const ENGLISH_HELLO_PREFIX = "Hello, "
+const SPANISH_HELLO_PREFIX = "Hola, "
+const FRENCH_HELLO_PREFIX = "Bonjour, "
+
+const ENDING_PREFIX = "!"
+
+// 公開したくない関数は先頭を小文字にする。
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case FRENCH:
+		prefix = FRENCH_HELLO_PREFIX
+	case SPANISH:
+		prefix = SPANISH_HELLO_PREFIX
+	default:
+		prefix = ENGLISH_HELLO_PREFIX
+	}
+	return
+}
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == spanish {
-		return spanishHelloPrefix + name + ending
-	}
-	return englishHelloPrefix + name + ending
+
+	prefix := greetingPrefix(language)
+
+	return prefix + name + ENDING_PREFIX
 }
 
 func main() {
