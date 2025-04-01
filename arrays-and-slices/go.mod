b40f5c2a0e6a01b0cd16961bdae2b952f1690e19
chihaya <takuchi.1071880@gmail.com>
2025-04-01 15:41:35 +0900

    add: 整数のスライスのスライスを受け取って、各スライスの和のスライスを返す機能の追加

---
+++ arrays-and-slices/go.mod
@@ -0,0 +1,3 @@
+module github.com/takuchi17/learn-go-with-tests/arrays-and-slices
+
+go 1.24.1
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -10,6 +11,17 @@ func TestSum(t *testing.T) {
 		want := 11
 
 		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("collections of any size", func(t *testing.T) {
+		got := SumAll([]int{1, 2, 3, 5}, []int{2, 3})
+		want := []int{11, 5}
+
+		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %d want %d", got, want)
 		}
 	})
