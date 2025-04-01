abefeded378386143bf5c127c36eaf9840093f7e
chihaya <takuchi.1071880@gmail.com>
2025-04-01 15:50:47 +0900

    update: SumAll関数を、最初の要素をのぞいたスライスの和を返す関数SumAllTailsに変更

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -16,10 +16,19 @@ func TestSum(t *testing.T) {
 	})
 }
 
-func TestSumAll(t *testing.T) {
-	t.Run("collections of any size", func(t *testing.T) {
-		got := SumAll([]int{1, 2, 3, 5}, []int{2, 3})
-		want := []int{11, 5}
+func TestSumAllTails(t *testing.T) {
+	t.Run("make the sums of slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3, 5}, []int{2, 3})
+		want := []int{10, 3}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("safety sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{2, 3})
+		want := []int{0, 3}
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %d want %d", got, want)
