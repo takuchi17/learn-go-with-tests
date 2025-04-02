25e3820c0446b84fe19c227c6e0bf4273e1558d5
chihaya <takuchi.1071880@gmail.com>
2025-04-02 17:40:25 +0900

    add: 円の周を求める処理を追加

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -1,12 +1,22 @@
 package main
 
+import "math"
+
 type Rectangle struct {
 	width  float64
 	height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.width + rectangle.height)
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.width + r.height)
+}
+
+type Circle struct {
+	radias float64
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.radias
 }
 
 func Area(rectangle Rectangle) float64 {
--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -5,11 +5,20 @@ import "testing"
 func TestPerimeter(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{10.0, 10.0}
-		got := Perimeter(rectangle)
+		got := rectangle.Perimeter()
 		want := 40.0
 
 		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+	t.Run("Cicle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Perimeter()
+		want := 62.83185307179586
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
 		}
 	})
 }
@@ -20,7 +29,7 @@ func TestArea(t *testing.T) {
 		got := Area(rectangle)
 		want := 60.0
 		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+			t.Errorf("got %g want %g", got, want)
 		}
 	})
 }
