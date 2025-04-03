08a46eb31ffb57f73920f13a1bdd9ba8084b74db
chihaya <takuchi.1071880@gmail.com>
2025-04-03 23:23:18 +0900

    refactor

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -4,19 +4,22 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	perimeterTests := []struct {
-		shape Shape
-		want  float64
+		name         string
+		shape        Shape
+		hasPerimeter float64
 	}{
-		{Rectangle{12, 6}, 36},
-		{Circle{10}, 62.83185307179586},
-		{Triangle{3, 4, 5}, 12},
+		{"Rectangle", Rectangle{12, 6}, 36},
+		{"Circle", Circle{10}, 62.83185307179586},
+		{"Triangle", Triangle{3, 4, 5}, 12},
 	}
 
 	for _, tt := range perimeterTests {
-		got := tt.shape.Perimeter()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Perimeter()
+			if got != tt.hasPerimeter {
+				t.Errorf("got %g want %g", got, tt.hasPerimeter)
+			}
+		})
 	}
 }
 
