61825cf31af033d531c9e494c9dec157ba795df4
chihaya <takuchi.1071880@gmail.com>
2025-04-05 02:31:02 +0900

    update: キーが存在しない場合の処理を追加追加

--- map/dictionary_test.go
+++ map/dictionary_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is a test"}
 
-	got := dictionary.Search("test")
-	want := "this is a test"
-	assertStrings(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is a test"
+		assertStrings(t, got, want)
+	})
 
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t *testing.T, got string, want string) {
