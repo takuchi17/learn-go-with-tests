e9a414de947020e4b5ebffb7c8c99f106ee8328b
chihaya <takuchi.1071880@gmail.com>
2025-04-05 02:50:33 +0900

    add: 追加機能の追加

--- map/dictionary_test.go
+++ map/dictionary_test.go
@@ -20,6 +20,22 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is a test")
+
+	want := "this is a test"
+	got, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
 func assertError(t *testing.T, got error, want error) {
 	t.Helper()
 
