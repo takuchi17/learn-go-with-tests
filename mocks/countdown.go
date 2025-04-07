0c3151c977a22be9e3f1c814dd0d072c33d244a7
chihaya <takuchi.1071880@gmail.com>
2025-04-07 09:45:56 +0900

    update: カウント中にスリープ

--- mocks/countdown.go
+++ mocks/countdown.go
@@ -4,19 +4,32 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
 
 const finalWord = "Go!"
 const countdownStart = 3
 const countdownEnd = 0
 
-func Countdown(buffer io.Writer) {
+func Countdown(buffer io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > countdownEnd; i-- {
-		fmt.Fprint(buffer, i)
+		fmt.Fprintln(buffer, i) // ln adds a newline
+		sleeper.Sleep()
 	}
-	fmt.Fprintln(buffer, finalWord) // ln adds a newline
+	fmt.Fprint(buffer, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
